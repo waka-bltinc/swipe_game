228de18e3e7c5ded03191a17ba6507ea59e976dc
ShoWaka <wakasho0507@gmail.com>
2024-04-22 21:55:24 +0900

    feat: PCで左右のスワイプを取得する機能を実装

--- js/sketch.js
+++ js/sketch.js
@@ -1,4 +1,10 @@
 
+let startX;
+let startY;
+let endX;
+let endY;
+let isSwiping = false;
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   background("gray");
@@ -15,17 +21,6 @@ function draw() {
   const GAME_AREA_WIDTH = min(window.innerWidth, 400);
   const GAME_AREA_HEIGHT = window.innerHeight;
 
-
-  // window.addEventListener('load', function () {
-  //   // スワイプ／フリック
-  //   // document.getElementById("content1").addEventListener('touchmove', logTouch);
-  // });
-
-  // function logTouch(event) {
-  //   console.log("X:" + event.touches[0].pageX);
-  //   console.log("Y:" + event.touches[0].pageY);
-  // }
-
   /**
    * 画面中央にゲーム領域を区別するための矩形を描画
    */
@@ -39,4 +34,41 @@ function draw() {
   );
 }
 
+/**
+ * マウスが押されたときに発火する
+ */
+function mousePressed() {
+  // スワイプ開始位置を記録
+  startX = mouseX;
+  startY = mouseY;
+  isSwiping = true;
+}
 
+/**
+ * マウスが離れたときに発火する
+ */
+function mouseReleased() {
+  // スワイプ終了位置を記録
+  endX = mouseX;
+  endY = mouseY;
+  isSwiping = false;
+
+  // スワイプの方向を計算
+  checkHorizontalSwipe();
+}
+
+/**
+ * スワイプの方向を計算する
+ */
+function checkHorizontalSwipe() {
+  const dx = endX - startX;
+  const distance = abs(dx);
+
+  if (distance > 50) { // スワイプと認識する最小距離
+    if (dx > 0) {
+      console.log("Right swipe");
+    } else {
+      console.log("Left swipe");
+    }
+  }
+}
